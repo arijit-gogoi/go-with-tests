d8f7d65bd8a306c44acc5031f0c79760dd49310f
Arijit Gogoi <arijitnewmail@gmail.com>
2023-06-27 14:26:15 +0530

    done: SumAllTails

--- 04-arrays-and-slices/arrays-and-slices.go
+++ 04-arrays-and-slices/arrays-and-slices.go
@@ -23,3 +23,17 @@ func SumAll(numsToSum ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	sums := []int{}
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
